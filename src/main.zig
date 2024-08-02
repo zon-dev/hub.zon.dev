60bab33a9cc718a27211d9bb8e673728eeaccae7
dravenk <longxianwen@outlook.com>
2024-08-02 17:19:30 +0800

    update init function

--- src/main.zig
+++ src/main.zig
@@ -2,34 +2,75 @@ const std = @import("std");
 const z = @import("zinc");
 
 pub fn main() !void {
-    var engine = try z.Engine.default();
-    // var engine = try z.Engine.new(8088);
+    var zinc = try z.Engine.init(.{
+        .port = 8081,
+        .addr = "0.0.0.0",
+    });
 
-    std.debug.print("Listening on: 127.0.0.1:{any}\n", .{engine.getPort()});
+    std.debug.print("Listening on: {any}\n", .{zinc.getAddress()});
 
-    var router = &engine.router;
-    try router.get("/hello", hello);
+    try zinc.use(.{
+        .prefix = "/",
+        // .handler_fn = cors,
+        .handler_fn = unauthorized,
+        
+    });
+
+    // engine.use(.{
+    //     .handler = fn(ctx: *z.Context, req: *z.Request, res: *z.Response) anyerror!void {
+    //         try ctx.next(req, res),
+    //     },
+    // });
+
+    var router = zinc.getRouter();
+    try router.get("/", hello);
     try router.post("/hi", hi);
     try router.add(std.http.Method.GET, "/ping", ping);
 
-    for (router.getRoutes().items) |r| {
-        std.debug.print("{s} | {s}\n", .{ @tagName(r.http_method), r.path });
-    }
+    // for (router.getRoutes().items) |r| {
+    //     std.debug.print("{s} | {s}\n", .{ @tagName(r.http_method), r.path });
+    // }
 
-    _ = try engine.run();
+    var catchers = zinc.getCatchers();
+    try catchers.put(.not_found, notFound);
+    // try catchers.put(.forbidden, forbidden);
+    // try catchers.put(.unauthorized, unauthorized);
+
+   try zinc.run();
 }
 
-fn hello(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
-    std.debug.print("Hello!\n", .{});
-    try res.send("Hello!");
+fn cors(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    _ = ctx;
 }
 
-fn hi(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
-    std.debug.print("hi!\n", .{});
-    try res.send("hi!");
+fn unauthorized(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    try ctx.HTML(.{
+        .status = .unauthorized,
+    },"<h1>401 Unauthorized</h1>" );
 }
 
-fn ping(_: *z.Context, _: *z.Request, res: *z.Response) anyerror!void {
-    std.debug.print("pong!\n", .{});
-    try res.send("pong!");
+fn forbidden(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    try ctx.HTML(.{
+        .status = .forbidden,
+    },"<h1>403 Forbidden</h1>" );
+}
+
+fn notFound(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    try ctx.HTML(.{
+        .status = .not_found,
+    },"<h1>404 Not Found</h1>" );
+}
+
+fn hello(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    try ctx.HTML(.{},"<h1>Hello World!</h1>" );
+}
+
+fn hi(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    try ctx.JSON(.{},.{
+        .message ="hi!",
+    });
+}
+
+fn ping(ctx: *z.Context, _: *z.Request, _: *z.Response) anyerror!void {
+    try ctx.Text(.{},"pong!" );
 }
